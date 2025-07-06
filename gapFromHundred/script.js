bb0b3542dc359a793f0ecd8cafc261b22b38e471
Aryan <itzaryansaini@gmail.com>
2025-07-06 11:20:29 +0530

    adding gapfromhunder file

---
+++ gapFromHundred/script.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+// Generate initial question when page loads
+// addition_questions(1 ,100);
+
+// Refresh button event listener
+
+document.getElementById('refreshBtn').addEventListener('click', () => {
+    for (let i = 1; i <= 30; i++) {
+        const num = Math.floor(Math.random() * 90) + 10;
+        let content = document.querySelector("div.content")
+        let html = `
+        <div class="question-answer">
+        <span class="question">
+        <span class="num">${num}</span> + <span class="answer">?</span> = 100 <button class="show_answer">Show</button>
+        </span>
+        </div>`
+        content.insertAdjacentHTML("beforeend", html)
+    }
+    let buttons = document.querySelectorAll("button.show_answer")
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            let parent = event.target.parentElement
+            let num = parent.querySelector(".num").textContent
+            parent.querySelector("span.answer").textContent = 100 - num
+            button.style.display = "none"
+        })
+    })
+});
+
+// Download button event listener
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    const content = document.getElementById('content');
+
+    html2canvas(content).then(canvas => {
+        // Create a temporary link to download the image
+        const link = document.createElement('a');
+        link.download = 'word-meaning.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+    });
+});
