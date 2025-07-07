1531aa2a49841601c546ae0b0edbd5aa0266f160
Aryan <itzaryansaini@gmail.com>
2025-07-07 12:45:27 +0530

    modifing word-meaning file to generate table with custom header

--- wordmeaning/script.js
+++ wordmeaning/script.js
@@ -6,20 +6,57 @@
 // Generate initial question when page loads
 // addition_questions(1 ,100);
 
+// let togleHeader = document.querySelector("button.togle_header")
+// togleHeader.addEventListener("click",(event)=>{
+//    let table = document.querySelector(".my-table").firstElementChild.firstElementChild
+//    table.innerHTML = ""
+//    console.log(table)
+// })
+
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 // Refresh button event listener
 document.getElementById('refreshBtn').addEventListener('click', () => {
+    let headers = document.querySelector("input#headers").value
+    if (headers !== "") {
+        let headers_arr = headers.split(",")
+        document.querySelector("table.my-table").insertAdjacentHTML("afterbegin", `<tr>
+                       <th>${capitalizeFirstLetter(headers_arr[0].trim())}</th>
+                       <th>${capitalizeFirstLetter(headers_arr[1].trim())}</th>
+                       <th>${capitalizeFirstLetter(headers_arr[2].trim())}</th>
+                       <th>${capitalizeFirstLetter(headers_arr[3].trim())}</th>
+                    </tr>`)
+    } else {
+        document.querySelector("table.my-table").insertAdjacentHTML("afterbegin", `<tr>
+                       <th>Word</th>
+                       <th>Meaning</th>
+                       <th>Word</th>
+                       <th>Meaning</th>
+                    </tr>`)
+    }
     let input = document.querySelector("#input").value
     let arr = JSON.parse(input)
-    for (let i = 0; i < arr.length; i=i+4) {
+    // let wordMeaning = document.querySelector('input[name="Word-Meaning"]:checked').value;
+    // if(wordMeaning == "yes"){
+    //     document.querySelector("table.my-table").insertAdjacentHTML("afterbegin" , `<tr>
+    //                    <th>Word</th>
+    //                    <th>Meaning</th>
+    //                    <th>Word</th>
+    //                    <th>Meaning</th>
+    //                 </tr>`)
+    // }
+    for (let i = 0; i < arr.length; i = i + 4) {
         let table = document.querySelector("table.my-table")
         let html = `
         <tr>
-            <td>${arr[i]}</td>
-            <td>${arr[i+1]}</td>
-            <td>${arr[i+2]}</td>
-            <td>${arr[i+3]}</td>
+            <td>${capitalizeFirstLetter(arr[i].trim())}</td>
+            <td>${capitalizeFirstLetter(arr[i + 1].trim())}</td>
+            <td>${capitalizeFirstLetter(arr[i + 2].trim())}</td>
+            <td>${capitalizeFirstLetter(arr[i + 3].trim())}</td>
         </tr>`
-        table.insertAdjacentHTML("beforeend",html)
+        table.insertAdjacentHTML("beforeend", html)
 
     }
 });
